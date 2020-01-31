3fafd495b4dff93697b3f33c1e2b9b45bb78a0c1
Vaultboy58 <58577918+Vaultboy58@users.noreply.github.com>
2020-01-30 20:18:36 -0600

    update

---
+++ script.js
@@ -0,0 +1,32 @@
+var numberofchars = prompt('how many chars would u like')
+
+var specialchars = confirm('do u want special chars') 
+
+var uppercase = confirm('do u want uppercase')
+
+var lowercase = confirm('do u want lowercase') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
