40657d2f4ef64e0c49bef3a8182e92b6d815d7c0
Jackson Strauss <58577918+Jastr0445@users.noreply.github.com>
2020-06-10 16:41:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,6 @@ function generatePassword(){
 // Add event listener to generate buttosn
 generateBtn.addEventListener("click", writePassword);
 
-
-
 var upperCasedChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 var lowerCasedChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
